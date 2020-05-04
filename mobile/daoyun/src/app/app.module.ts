e4510c8c95a25726a706833eb779a75186592343
xLuge <37794367+xLuge@users.noreply.github.com>
2020-05-04 18:07:04 +0800

    登录注册

--- mobile/daoyun/src/app/app.module.ts
+++ mobile/daoyun/src/app/app.module.ts
@@ -8,11 +8,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LoginPage } from './pages/login/login.page';
+import { LoginPageModule } from './pages/login/login.module';
+import {HttpClientModule} from "@angular/common/http"
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,
+  ],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule],
   providers: [
     StatusBar,
     SplashScreen,
--- mobile/daoyun/src/app/pages/pages.module.ts
+++ mobile/daoyun/src/app/pages/pages.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginPageModule } from './login/login.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    LoginPageModule
   ]
 })
 export class PagesModule { }

dfcbc57ee1592a36858b33e8d3d5125274460fd3
lililiwh <522300620@qq.com>
2020-05-19 08:54:07 +0800

    role

--- server/src/main/java/com/hxskyun/mapper/RoleMapper.java
+++ server/src/main/java/com/hxskyun/mapper/RoleMapper.java
@@ -9,20 +9,67 @@ import java.util.List;
 
 @Mapper
 public interface RoleMapper {
+    /**
+     * 根据主键删除
+     *
+     * @param roleId
+     * @return
+     */
 
     int deleteByPrimaryKey(Integer roleId);
+    /**
+     * 插入
+     *
+     * @param record
+     * @return
+     */
 
     int insert(Role record);
+    /**
+     * 插入
+     *
+     * @param record
+     * @return
+     */
 
     int insertSelective(Role record);
+    /**
+     * 根据主键查询
+     *
+     * @param roleId
+     * @return
+     */
 
     Role selectByPrimaryKey(Integer roleId);
+    /**
+     * 根据Selective更新
+     *
+     * @param record
+     * @return
+     */
 
     int updateByPrimaryKeySelective(Role record);
+    /**
+     * 根据主键更新
+     *
+     * @param record
+     * @return
+     */
+
 
     int updateByPrimaryKey(Role record);
+    /**
+     * 查询所有
+     *
+     * @return
+     */
 
     List<Role> selectAll();
+    /**
+     * 根据角色名查询
+     *
+     * @return
+     */
 
     Role selectByRoleName(String name);
 }
